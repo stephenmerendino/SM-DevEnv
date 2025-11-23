1c83a1b047bed464ef8b4777f775af23adff44a3
Stephen Merendino <StephenMerendino@gmail.com>
2025-11-23 17:19:20 -0600

    Modified gitignore to include after/plugin dir

---
+++ neovim/nvim/after/plugin/colors.lua
@@ -0,0 +1,10 @@
+function SetColorScheme(color)
+    color = color or "gruvbox"
+    vim.cmd.colorscheme(color)
+    vim.o.background = "dark" -- or "light" for light mode
+
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+SetColorScheme()
