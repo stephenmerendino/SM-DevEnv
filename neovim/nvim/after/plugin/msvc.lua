5ae7d923fe82987d0b0249cd87d9a76f7fbce6e3
Stephen Merendino <smerendino@naughtydog.com>
2025-11-25 09:35:45 -0800

    Finally got building cpp files how I like it from within neovim

--- neovim/nvim/after/plugin/msvc.lua
+++ neovim/nvim/after/plugin/msvc.lua
@@ -1,14 +1,59 @@
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = {"cpp", "cxx", "cc", "h", "hpp"},  -- add extensions as needed
+    pattern = {"cpp", "cxx", "cc", "c"},  -- add extensions as needed
     callback = function()
-        vim.bo.makeprg = "build.bat"
-        vim.bo.errorformat = "%f(%l): error C%n: %m,%f(%l): warning C%n: %m"
-    end,
+        local fname = vim.api.nvim_buf_get_name(0)
+        local fdir = vim.fn.fnamemodify(fname, ":p:h")
+        local dir = fdir        
+
+        -- Function to join paths (since Windows uses \ while Neovim prefers /)
+        local function join_path(...)
+            return table.concat({...}, package.config:sub(1,1))
+        end
+
+        -- Upward search for build.bat
+        local buildbat = nil
+        while dir and dir ~= "" do
+            local candidate = join_path(dir, "build.bat")
+            if vim.fn.filereadable(candidate) == 1 then
+                buildbat = candidate
+                break
+            end
+            local parent = vim.fn.fnamemodify(dir, ":h")
+            if parent == dir then break end  -- at root
+            dir = parent
+        end
+
+        if buildbat then
+            -- Always quote Windows paths!
+            vim.bo.makeprg = '"' .. buildbat .. '"'
+        else
+            local filename = vim.fn.expand('%:t')
+            local basename = vim.fn.fnamemodify(filename, ":r")
+            local builddir = join_path(fdir, "Build")
+            local exe_path = join_path(builddir, basename .. ".exe")
+            local pdb_path = join_path(builddir, basename .. ".pdb")
+            local obj_path = join_path(builddir, basename .. ".obj")
+            vim.bo.makeprg = table.concat({
+                'mkdir "' .. builddir .. '" 2>nul',
+                'cl.exe /nologo /EHsc /Od /Zi ' ..
+                '/Fe"' .. exe_path .. '" ' ..
+                '/Fd"' .. pdb_path .. '" ' ..
+                '/Fo"' .. obj_path .. '" "' .. fname .. '"'
+            }, " && ")
+        end
+
+        vim.bo.errorformat = table.concat({
+            '%f(%l): error C%n: %m',
+            '%f(%l): fatal error C%n: %m',
+            '%f(%l): warning C%n: %m'
+        }, ',')
+    end
 })
 
 vim.api.nvim_create_autocmd("QuickFixCmdPost", {
-    pattern = { "[^l]*" },
+    pattern = { "make" },
     callback = function()
+        print("Should Open cwindow")
         vim.cmd("cwindow")
     end,
 })
