a4ecbf3a5cb5673cadd851650d054b815052cc52
Stephen Merendino <StephenMerendino@gmail.com>
2025-12-03 21:01:10 -0600

    Reenable vim command auto completion

--- neovim/nvim/after/plugin/nvim-cmp.lua
+++ neovim/nvim/after/plugin/nvim-cmp.lua
@@ -62,12 +62,12 @@ require("cmp_git").setup() ]]--
 -- })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
--- cmp.setup.cmdline(':', {
---     mapping = cmp.mapping.preset.cmdline(),
---     sources = cmp.config.sources({
---         { name = 'path' }
---     }, {
---         { name = 'cmdline' }
---     }),
---     matching = { disallow_symbol_nonprefix_matching = false }
--- })
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
+})
