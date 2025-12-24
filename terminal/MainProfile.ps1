22f991b933cbc8a3bb2f89e7e3bb32b5ac07dcf7
Stephen Merendino <StephenMerendino@gmail.com>
2025-12-23 21:54:58 -0600

    Setup basic wezterm config and improved powershell startup script

--- terminal/MainProfile.ps1
+++ terminal/MainProfile.ps1
@@ -1,9 +1,23 @@
 Set-ExecutionPolicy RemoteSigned
 oh-my-posh init pwsh --config "https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/gruvbox.omp.json" | Invoke-Expression
 
+$Workspace = "$env:USERPROFILE\Workspace"
+
+function Open-Nvim-Workspace {
+    nvim $Workspace
+}
+
+function Go-To-Work {
+    Set-Location $Workspace
+}
+
 new-alias -name n nvim
-. "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1" -Latest -Arch amd64 -HostArch amd64
-Set-Location -Path $env:USERPROFILE\Workspace
+new-alias -name g git
+new-alias -name work Go-To-Work
+new-alias -name nw Open-Nvim-Workspace 
+
+. "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1" -Latest -Arch amd64 -HostArch amd64 1>$null
+Set-Location -Path $Workspace 
 
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
