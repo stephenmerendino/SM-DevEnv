27534fa99aff6908b1552ba4a9d46ade53a540a2
Stephen Merendino <StephenMerendino@gmail.com>
2026-01-03 10:25:10 -0600

    Update komorebi to use my own applications.json and set my Workbench.exe to be floating

--- komorebi/Setup.bat
+++ komorebi/Setup.bat
@@ -1,10 +1,15 @@
 @echo off
 
 set appName=Komorebi
-set fileName=komorebi.json
 
 echo [%appName%] Setting up development environment
 
+set fileName=komorebi.json
+set targetConfigFile="%USERPROFILE%\%fileName%"
+IF EXIST %targetConfigFile% DEL %targetConfigFile%
+mklink %targetConfigFile% %~dp0%fileName%
+
+set fileName=komorebi_applications.json
 set targetConfigFile="%USERPROFILE%\%fileName%"
 IF EXIST %targetConfigFile% DEL %targetConfigFile%
 mklink %targetConfigFile% %~dp0%fileName%
