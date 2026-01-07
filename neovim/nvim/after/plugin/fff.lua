4203d293c9d5266df4e868b1180be829722aeeca
stephenmerendino <stephenmerendino@gmail.com>
2026-01-06 16:39:31 -0800

    More dev config for komorebi and yasb

--- neovim/nvim/after/plugin/fff.lua
+++ neovim/nvim/after/plugin/fff.lua
@@ -1,5 +1,10 @@
 local fff = require('fff')
-fff.setup()
+fff.setup({
+    title = "Find File",
+    lazy_sync = false,
+    max_threads = 4,
+    max_results = 100
+})
 
 vim.keymap.set('n', '<leader>pf', function() fff.find_files() end, { desc = 'FFF find all files' })
 vim.keymap.set('n', '<C-p>', function() fff.find_in_git_root() end, { desc = 'FFF find git files' })
